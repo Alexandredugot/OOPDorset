871a70f742482d2402be583ba9f367c2677383aa
Alexandredugot <thomandflamming@gmail.com>
2022-03-24 14:59:22 +0000

    background

--- salut/src/com/mathmaurer/jeu/Scene.java
+++ salut/src/com/mathmaurer/jeu/Scene.java
@@ -9,11 +9,13 @@ public class Scene extends JPanel {
 
     private ImageIcon icoFond;
     private Image imgfond1;
+    private Image imgfond2;
 
     private ImageIcon icoGame;
     private Image imgGame;
 
     private int xFond1;
+    private int xFond2;
 
     private int dx;
 
@@ -21,9 +23,11 @@ public class Scene extends JPanel {
     public Scene(){
         super();
         this.xFond1 = -50;
+        this.xFond2 = 750;
         this.dx = 0;
         icoFond = new ImageIcon(getClass().getResource("/Image/fondEcran.png"));
         this.imgfond1 = this.icoFond.getImage();
+        this.imgfond2 = this.icoFond.getImage();
         icoGame = new ImageIcon(getClass().getResource("/Image/marioArretDroite.png"));
         this.imgGame = this.icoGame.getImage();
 
@@ -45,7 +49,25 @@ public class Scene extends JPanel {
     }
 
     public void Movefond(){
+
         this.xFond1 = this.xFond1 - this.dx;
+        this.xFond2 = this.xFond2 - this.dx;
+        if(this.xFond1 == -800)
+        {
+            this.xFond1 = 800;
+        }
+        else if(this.xFond2 == 800)
+        {
+            this.xFond2 = 800;
+        }
+        else if(this.xFond1 == 800)
+        {
+            this.xFond2 = -800;
+        }
+        else if(this.xFond2 == 800)
+        {
+            this.xFond2 = -800;
+        }
     }
 
     public void paintComponent(Graphics g){
@@ -54,6 +76,7 @@ public class Scene extends JPanel {
 
         this.Movefond();
         g2.drawImage(this.imgfond1,this.xFond1,0,null);
+        g2.drawImage(this.imgfond2,this.xFond2,0,null);
         g2.drawImage(imgGame,300,245,null);
     }
 }
