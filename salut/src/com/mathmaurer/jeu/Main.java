b21012ddc2fc5df77e2d52808c3b75a72f475e43
Vincent-GL <vincent.goguyer-lalande@epita.fr>
2022-03-24 17:11:46 +0000

    Menu update

--- salut/src/com/mathmaurer/jeu/Main.java
+++ salut/src/com/mathmaurer/jeu/Main.java
@@ -1,6 +1,7 @@
 package com.mathmaurer.jeu;
 
 import javax.swing.JFrame;
+import java.util.EventListener;
 
 public class Main {
 
@@ -19,9 +20,15 @@ public class Main {
         fenetre.setResizable(true);
         fenetre.setAlwaysOnTop(true);
 
-     /*   Menu menu = new Menu();
-        fenetre.setContentPane(menu);*/
+        /*Menu menu = new Menu();
+        fenetre.setContentPane(menu);
+        fenetre.setVisible(true);
 
+        while(menu.play !=1){
+            System.out.println(menu.play);
+        }
+        fenetre.setVisible(false);
+        fenetre.remove(menu);*/
         scene = new Scene();
         fenetre.setContentPane(scene);
         fenetre.setVisible(true);
--- salut/src/com/mathmaurer/jeu/Menu.java
+++ salut/src/com/mathmaurer/jeu/Menu.java
@@ -4,22 +4,35 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
-public class Menu extends JPanel  {
+public class Menu extends JPanel implements ActionListener {
+    public int play =0;
     JButton button;
+    JLabel label;
     //créer public attribut state déclarant l'état du bouton appuyé
     Menu(){
+
+        ImageIcon background = new ImageIcon(getClass().getResource("/Image/marioArretDroite.png"));
+        label= new JLabel();
+        label.setIcon(background);
+        label.setBounds(0,0,700,300);
         button = new JButton();
         button.setBounds(250,100,200,50);
         this.setLayout(null);
-       // this.setSize(100,100);
-       // this.setVisible(true);
         this.add(button);
-        button.addActionListener(e->System.out.println("hello"));
+        button.addActionListener(this);
+        button.setText("Play");
+        button.setFocusable(false);
+        button.setBackground(Color.red);
+        button.setForeground(Color.CYAN);
+        this.setBackground(Color.ORANGE);
+        this.add(label);
+        label.setVisible(true);
     }
-   /* @Override
+    @Override
     public void actionPerformed(ActionEvent e){
         if(e.getSource()==button){
             System.out.println("here");
+            play=1;
         }
-    }*/
+    }
 }
